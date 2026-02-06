3c2ed26a1446c5d4a8f5cb1732fb9803248a2da6
Aj051201 <ajayverma0138@gmail.com>
2026-02-06 17:55:34 +0530

    update publication

--- src/api/publications.ts
+++ src/api/publications.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { API_BASE } from "../config";
-import { authHeader } from "/Assessment/assessment-test-frontend/src/api/auth";
+import { authHeader } from "./auth";
 
 export const fetchPublications = () =>
   axios.get(`${API_BASE}/publications`, { headers: authHeader() });
@@ -9,10 +9,18 @@ export const fetchPublicPublished = () =>
   axios.get(`${API_BASE}/publications/public`);
 
 export const createPublication = (data: any) =>
-  axios.post(`${API_BASE}/publications`, { publication: data }, { headers: authHeader() });
+  axios.post(
+    `${API_BASE}/publications`,
+    { publication: data },
+    { headers: authHeader() }
+  );
 
 export const updatePublication = (id: number, data: any) =>
-  axios.patch(`${API_BASE}/publications/${id}`, { publication: data }, { headers: authHeader() });
+  axios.patch(
+    `${API_BASE}/publications/${id}`,
+    { publication: data },
+    { headers: authHeader() }
+  );
 
 export const deletePublication = (id: number) =>
   axios.delete(`${API_BASE}/publications/${id}`, { headers: authHeader() });
